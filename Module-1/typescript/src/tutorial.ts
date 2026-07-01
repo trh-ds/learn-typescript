f79dbc07fa71c818dcf3ea3c25785b7ec9e2d843
Tirth <trhatwork@gmail.com>
2026-07-01 16:30:49 +0530

    Arrays having types as object

--- Module-1/typescript/src/tutorial.ts
+++ Module-1/typescript/src/tutorial.ts
@@ -19,3 +19,16 @@ let car:{brand:string,year:number} = {
     year:2020
 }
 console.log(car)
+
+
+//Arrays + Objects 
+
+let book = {title:"book",cost:20}
+let book2 = {title:"book2",cost:50}
+let book3 = {title:"book3",cost:30}
+
+let items:{title:string;cost?:number}[] = [];
+items.push(book);
+items.push(book2);
+items.push(book3);
+console.log(items);
