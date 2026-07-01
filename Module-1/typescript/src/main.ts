279ae1dcdcd8a93024b3b23c31e1445eba57a91b
Tirth <trhatwork@gmail.com>
2026-07-01 16:22:50 +0530

    Arrays can have multiple types

--- Module-1/typescript/src/main.ts
+++ Module-1/typescript/src/main.ts
@@ -3,7 +3,7 @@ import typescriptLogo from './assets/typescript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.ts'
-
+import './tutorial.ts'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="center">
   <div class="hero">
---
+++ Module-1/typescript/src/tutorial.ts
@@ -0,0 +1,11 @@
+// let name:string = "Tirth"
+
+// console.log(name)
+
+// Arrays
+
+let princes : number[] = [100, 200, 300, 400, 500]
+
+console.log(princes)
+
+let array : (string | boolean)[]=["Apple",true]
