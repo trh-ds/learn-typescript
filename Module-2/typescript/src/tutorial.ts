0cb7c55a698d30a8735a304fcc86bea913045f43
Tirth <trhatwork@gmail.com>
2026-07-01 17:21:01 +0530

    Adding types to func params

---
+++ Module-2/typescript/src/tutorial.ts
@@ -0,0 +1,20 @@
+//readonly keyword 
+
+
+// let book = {title:"book",cost:20}
+// let book2 = {title:"book2",cost:50}
+// let book3 = {title:"book3",cost:30}
+
+// let items:{ readonly title:string;cost?:number}[] = [];
+// items.push(book);
+// items.push(book2);
+// items.push(book3);
+// console.log(items);
+
+//FUNCTIONS
+
+
+function greet(name:string) {
+    console.log(`Hello ${name}`)
+}
+greet("Alice");
