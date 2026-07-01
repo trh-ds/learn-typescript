f53d32afe3b5918a2992067f31ff5f3b1509eeaf
Tirth <trhatwork@gmail.com>
2026-07-01 16:27:28 +0530

    Types for objects

--- Module-1/typescript/src/tutorial.ts
+++ Module-1/typescript/src/tutorial.ts
@@ -4,8 +4,18 @@
 
 // Arrays
 
-let princes : number[] = [100, 200, 300, 400, 500]
+// let princes : number[] = [100, 200, 300, 400, 500]
 
-console.log(princes)
+// console.log(princes)
 
-let array : (string | boolean)[]=["Apple",true]
+// let array : (string | boolean)[]=["Apple",true]
+// console.log(array)
+
+
+//Objects 
+
+let car:{brand:string,year:number} = {
+    brand:"BMW",
+    year:2020
+}
+console.log(car)
